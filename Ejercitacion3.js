70b72e2eb05505456916cd127e7df3c1c2c366e5
Sebastián Alejandro González <sebastian.alejandro.gonzalez@outlook.com>
2022-11-10 09:31:18 -0300

    Final Ejercitación 3

--- Ejercitacion3.js
+++ Ejercitacion3.js
@@ -64,26 +64,71 @@ b. apellido
 c. sueldoActual
 d. porcentajeAumento */
 
-let nombre = 'Juan Ramón'; // creo variable nombre
+/* let nombre = 'Juan Ramón'; // creo variable nombre
 let apellido = 'Gioconda'; // creo variable apellido
 let sueldoActual = 100000; // creo variable sueldo actual
-let porcentajeAumento = 20; // creo variable procentaje de aumento
+let porcentajeAumento = 20; // creo variable procentaje de aumento */
 
 
 /* 3. Almacenar en una variable el “calculoAumento”. Esta contendrá el resultado de la
 operación del aumento del trabajador (sueldoActual * porcentajeAumento)/ 100). */
 
-let calculoAumento = (sueldoActual * porcentajeAumento)/ 100; // se crea la variable de cálculo
+/* let calculoAumento = (sueldoActual * porcentajeAumento)/ 100; // se crea la variable de cálculo */
 
 /* 4. En otra variable “nuevoSueldo”, almacena el resultado del cálculo del nuevo
 sueldo. */
 
-let nuevoSueldo = sueldoActual + calculoAumento; // se crea la variable que contiene el nuevo sueldo
+/* let nuevoSueldo = sueldoActual + calculoAumento; // se crea la variable que contiene el nuevo sueldo */
 
 /* 5. Usando el console.log() y concatenando string, muestra en la consola de Visual
 Studio Code los contenidos de cada una de las variables: */
 
-console.log('Estimado ' + nombre + ' ' + apellido +': ')
+/* console.log('Estimado ' + nombre + ' ' + apellido +': ')
 console.log('Su sueldo actual es: $'+ sueldoActual);
 console.log('El porcentaje de aumento es: ' + porcentajeAumento + '%');
-console.log('Su nuevo sueldo es: $' + nuevoSueldo);
+console.log('Su nuevo sueldo es: $' + nuevoSueldo); */
+
+/* Desafío extra (opcional): */
+
+/* Instrucciones
+1. En la carpeta ya creada crear un nuevo archivo JavaScript (.js).
+2. En el archivo creado debes desarrollar un código para lograr efectuar operaciones
+aritméticas. Para ello, declarar dos variables, otorgando el nombre y los valores
+numéricos que consideres. Ejemplo: let numero1 = 16; */
+
+/* let numero1 = 50;
+let numero2 = 20; */
+
+/* 3. Usando el console.log() mostrar al usuario los resultados de las siguientes
+operaciones aritméticas, en función de los valores asignados a las variables:
+I. Suma
+II. Resta
+III. Multiplicación
+IV. División
+V. Módulo */
+
+/* let suma = numero1 + numero2; // creo la variable suma
+let resta = numero1 - numero2; // creo la variable resta
+let multiplicacion = numero1 * numero2;// creo la variable multiplicación
+let division = numero1 / numero2;// creo la variable división
+let modulo = numero1 % numero2; // creo la variable módulo
+
+console.log('La suma de nro1 y nro2 es: '+ suma);
+console.log('La resta de nro1 y nro2 es: '+ resta);
+console.log('La multiplicación de nro1 y nro2 es: '+ multiplicacion);
+console.log('La división de nro1 y nro2 es: '+ division);
+console.log('El módulo de nro1 y nro2 es: '+ modulo); */
+
+/* VI. ¿Cual de los valores es mayor? Si el valor es mayor, mostrará un valor boolean true.
+De lo contrario mostrará false */
+
+/* let mayor = numero1>numero2
+
+console.log('Nro 1 es mayor que nro2: ' + mayor) */
+
+/* VII. ¿Cual de los valores es menor? Si el valor es menor mostrará un valor boolean true.
+De lo contrario mostrará false */
+
+/* let menor = numero1<numero2
+
+console.log('Nro 1 es menor que nro2: ' + menor) */
