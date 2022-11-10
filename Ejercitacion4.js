51af107f340f04f375dec817b7fe76e2ad0c5da5
Sebastián Alejandro González <sebastian.alejandro.gonzalez@outlook.com>
2022-11-10 16:51:33 -0300

    2da carga Ejercicio 4

--- Ejercitacion4.js
+++ Ejercitacion4.js
@@ -8,8 +8,7 @@ a. administrador
 b. asistente
 c. invitado. */
 
-let perfil = 'Administrador'// creo variable perfil
-let administrador = toUpperCase('administrador' || 'ADMINISTRADOR' || 'Administrador')
+/* let perfil = 'Administrador'// creo variable perfil */
 
 /* Es importante tener en cuenta que se debe mostrar un único mensaje, utilizando el
 console.log():
@@ -26,16 +25,54 @@ mensaje: “Usted sólo tiene permisos de consultar datos”.
 E. Si se especifica un valor diferente a la variable perfil entonces se debe mostrar
 este mensaje: “Debe especificar un perfil válido”. */
 
-switch (perfil) {
-    case administrador:
+/* switch (perfil.toLowerCase) {
+    case 'administrador':
         console.log('Usted tiene todos los privilegios de uso del sistema');
         break;
-    case 'asistente' || 'ASISTENTE' || 'Asistente':
+    case 'asistente':
         console.log('Usted sólo tiene permisos de registrar, modificar y consultar datos');
         break;
-    case 'invitado' || 'INVITADO' || 'Invitado':
+    case 'invitado':
         console.log('Usted sólo tiene permisos de consultar datos');
     default:
         console.log('Debe especificar un perfil válido');
         break;
-}
+} */
+
+/* Micro desafío 2: */
+
+/* Instrucciones
+1. En la carpeta ya creada crea un nuevo archivo JavaScript (.js).
+2. Desarrolla el programa Ajuste tarifario de energía eléctrica. Utilizando el operador
+condicional ternario, queremos determinar cuánto va a pagar un hogar según su
+consumo. Veamos los pasos a seguir...
+a. Crear una variable “pagoMes” y asignarle un pago actual de energía
+eléctrica por mes.
+b. Crear otra variable “consumoKWH” y asignarle un consumo mensual de
+Kilovatios hora de consumo mensual por el hogar.
+c. Si en el hogar se consumen más de 300 kwh por mes, entonces al pago actual
+se le incrementará un 20%.
+d. En el caso contrario se le mantendrá el subsidio, es decir que no tendrá ningún
+aumento.. */
+
+/* let pagoMes = 12000;
+
+let consumoKWH = 250;
+
+let porcentajeIncremento = 1.20;
+
+let pagoIncremento = pagoMes * porcentajeIncremento
+
+let pagoFinal = consumoKWH>300 ? pagoIncremento : pagoMes  */
+
+/* 3. Una vez obtenido el monto del pago por consumo mostrar al usuario este mensaje:
+
+“Debido a que su hogar tuvo un consumo de 450kwh, en base al ajuste tarifario
+(hogares con consumo mayor a 300kwh por mes tendrán un aumento del 20%),
+cumplimos con informarle que se ha ajustado el total a pagar, que será de $14400” */
+
+/* console.log('Debido a que su hogar tuvo un consumo de ' + consumoKWH +', en base al ajuste tarifario hogares con consumo mayor a 300kwh '+ 
+     + 'por mes tendrán un aumento del ' + porcentajeIncremento + '%'
+    + 'cumplimos con informarle que se ha ajustado el total a pagar, que será de ' + pagoFinal) */
+
+    
